9246e44cfb438de81c69371fc0252f06bba1bca6
musafir928 <uganoghli@gmail.com>
2019-12-06 03:47:34 +0100

    fix logged user request sent issue

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { sendFriendRequest } from '../../actions/auth';
-import { socket } from '../../utils/socketClient';
-import moment from 'moment';
-import Moment from 'react-moment';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { sendFriendRequest } from "../../actions/auth";
+import { socket } from "../../utils/socketClient";
+import moment from "moment";
+import Moment from "react-moment";
 
 const ProfileItem = ({
   sendFriendRequest,
-  auth,
   profile: {
     user: { _id, name, avatar },
     status,
     company,
     location,
-    skills,
+    skills
   },
-  auth: { user, loading, isAuthenticated },
+  auth: { user, loading, isAuthenticated }
 }) => {
   const handleClick = e => {
     e.preventDefault();
@@ -27,34 +26,40 @@ const ProfileItem = ({
   const Button = () => {
     const isFriend = user.friendsList.filter(friend => friend.friendId === _id);
     const isRequested = user.request.filter(req => req.userId === _id);
-
     const isSent = user.sentRequest.filter(sendReq => sendReq.userId === _id);
+    const isLoggedUser = user._id === _id;
 
-    if (isAuthenticated) {
+    if (isAuthenticated && !isLoggedUser) {
       if (isFriend.length > 0) {
         return (
-          <p className="text-primary my-1">
-            <i class="fas fa-clock"></i> Friends since:{' '}
-            <Moment format="DD/MM/YYYY HH:mm:ss">{moment.utc(isFriend.date)}</Moment>
+          <p className='text-primary my-1'>
+            <i class='fas fa-clock'></i> Friends since:{" "}
+            <Moment format='DD/MM/YYYY HH:mm:ss'>
+              {moment.utc(isFriend.date)}
+            </Moment>
           </p>
         );
       }
 
       if (isRequested.length > 0) {
         return (
-          <p className="text-primary my-1">
-            <i className="fas fa-clock"></i> Requested at:{' '}
-            <Moment format="DD/MM/YYYY HH:mm:ss">{moment.utc(isRequested.date)}</Moment>
+          <p className='text-primary my-1'>
+            <i className='fas fa-clock'></i> Requested at:{" "}
+            <Moment format='DD/MM/YYYY HH:mm:ss'>
+              {moment.utc(isRequested.date)}
+            </Moment>
           </p>
         );
       }
 
       if (isSent.length > 0) {
         return (
-          <p className="text-primary my-1">
-            {' '}
-            <i className="fas fa-clock"></i> Request sent at: {'   '}
-            <Moment format="DD/MM/YYYY HH:mm:ss">{moment.utc(isSent.date)}</Moment>
+          <p className='text-primary my-1'>
+            {" "}
+            <i className='fas fa-clock'></i> Request sent at: {"   "}
+            <Moment format='DD/MM/YYYY HH:mm:ss'>
+              {moment.utc(isSent.date)}
+            </Moment>
           </p>
         );
       }
@@ -64,8 +69,8 @@ const ProfileItem = ({
       }
 
       return (
-        <button className="btn btn-success" onClick={handleClick}>
-          <i className="fas fa-user-plus"></i> Send Friend Request
+        <button className='btn btn-success' onClick={handleClick}>
+          <i className='fas fa-user-plus'></i> Send Friend Request
         </button>
       );
     }
@@ -73,23 +78,23 @@ const ProfileItem = ({
   };
 
   return (
-    <div className="profile bg-light">
-      <img src={avatar} alt="" className="round-img" />
+    <div className='profile bg-light'>
+      <img src={avatar} alt='' className='round-img' />
       <div>
         <h2>{name}</h2>
         <p>
           {status} {company && <span> at {company}</span>}
         </p>
-        <p className="my-1">{location && <span>{location}</span>}</p>
-        <Link to={`/profile/${_id}`} className="btn btn-primary">
+        <p className='my-1'>{location && <span>{location}</span>}</p>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
           View Profile
         </Link>
         <Button />
       </div>
       <ul>
         {skills.slice(0, 4).map((skill, index) => (
-          <li key={index} className="text-primary">
-            <i className="fas fa-check" /> {skill}
+          <li key={index} className='text-primary'>
+            <i className='fas fa-check' /> {skill}
           </li>
         ))}
       </ul>
@@ -100,10 +105,10 @@ const ProfileItem = ({
 ProfileItem.propTypes = {
   profile: PropTypes.object.isRequired,
   isAuthenticated: PropTypes.bool,
-  sendFriendRequest: PropTypes.func.isRequired,
+  sendFriendRequest: PropTypes.func.isRequired
 };
 const mapStateToProps = state => ({
-  auth: state.auth,
+  auth: state.auth
 });
 
 export default connect(mapStateToProps, { sendFriendRequest })(ProfileItem);
